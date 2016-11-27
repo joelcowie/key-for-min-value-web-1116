d7fdecd39c93e3f51c01b1369e28b90361beb55b
Joel Cowie <joel.a.cowie@gmail.com>
2016-11-27 02:35:16 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,12 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  return nil if name_hash.length == 0
+  invert_hash = name_hash.invert
+  until invert_hash.length == 1
+    max = invert_hash.max
+    invert_hash.delete_if{|k, v| k == max[0]}
+  end
+  array = invert_hash.first
+  return array[1]
 end
